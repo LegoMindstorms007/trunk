0a56d1f8ef2eaa47856d53c6f659ab687e9d2d99
Dominik Muth <dominik.muth@student.kit.edu>
2015-01-27 15:12:55 +0100

    added Line folower

---
+++ Gruppe7/src/Test/LineFolower.java
@@ -0,0 +1,54 @@
+package Test;
+
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+import RobotMovement.TrackSuspension;
+
+public class LineFolower implements Runnable {
+
+	private static final int LINE_VALUE = 35;
+	LightSensor light;
+	TrackSuspension track;
+
+	public LineFolower(SensorPort portOfLightSensor) {
+		light = new LightSensor(portOfLightSensor);
+
+		track = new TrackSuspension();
+	}
+
+	@Override
+	public void run() {
+
+		while (true) {
+			if (light.getLightValue() >= LINE_VALUE) {
+				track.forward();
+			} else {
+				track.stop();
+				searchTrack();
+			}
+		}
+	}
+
+	private void searchTrack() {
+		int angle = 5;
+		int i = 0;
+		while (light.getLightValue() < LINE_VALUE) {
+			if (i == 0) {
+				track.pivotAngleLeft(5);
+			} else {
+				track.pivotAngleRight(angle);
+			}
+			angle += 5;
+		}
+	}
+
+	private void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
