13641963cd57424dd273125cf86bd1e16bd26676
Dominik Muth <dominik.muth@student.kit.edu>
2015-01-27 17:33:50 +0100

    added test features

--- Gruppe7/src/RobotMovement/UltrasoundArm.java
+++ Gruppe7/src/RobotMovement/UltrasoundArm.java
@@ -1,26 +1,26 @@
 package RobotMovement;
+
 import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 
-
 public class UltrasoundArm {
 	private NXTRegulatedMotor motor;
-	final int LEFTPOSITION = 65;
-	final int RIGHTPOSITION = -65;
+	final int LEFTPOSITION = 0;
+	final int RIGHTPOSITION = 0;
 	final int CENTERPOSITION = 0;
+
 	public UltrasoundArm() {
 		motor = Motor.C;
-		motor.setSpeed(60);
 	}
-	
+
 	public void turnToLeft() {
 		motor.rotateTo(LEFTPOSITION);
 	}
-	
+
 	public void TurnToRight() {
 		motor.rotateTo(RIGHTPOSITION);
 	}
-	
+
 	public void center() {
 		motor.rotateTo(CENTERPOSITION);
 	}
