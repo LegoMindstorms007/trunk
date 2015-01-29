36c06813e644620bc69e22ba09c36046e134319c
Mark <uycvo@student.kit.edu>
2015-01-29 14:23:57 +0100

    Improved Start

--- Gruppe7/src/Test/Demo.java
+++ Gruppe7/src/Test/Demo.java
@@ -1,7 +1,11 @@
 package Test;
 
 import RobotMovement.SensorArm;
+import RobotMovement.UltrasoundSensor;
 import lejos.nxt.Button;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.comm.RConsole;
 public class Demo {
 
 	public static void main(String[] args) {
@@ -11,13 +15,26 @@ public class Demo {
 	//	new Thread(bridge).start();
 		//LineFolower follower = new LineFolower(SensorPort.S4);
 		//new Thread(follower).start();
-		BridgeDriving bridge = new BridgeDriving();
-		new Thread(bridge).start();
+	//	BridgeDriving bridge = new BridgeDriving();
+	//	new Thread(bridge).start();
 		// Labyrinth maze = new Labyrinth();
 		// new Thread(maze).start();
+		RConsole.openBluetooth(0);
+		Start start = new Start();
+		new Thread(start).start();
 		Button.waitForAnyPress();
 		//follower.halt();
 		// test.halt();
-		bridge.halt();
+	//	bridge.halt();
+		start.halt();
+		
+	}
+	
+	private static void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- Gruppe7/src/Test/Start.java
+++ Gruppe7/src/Test/Start.java
@@ -6,6 +6,7 @@ import RobotMovement.UltrasoundSensor;
 import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.comm.RConsole;
 
 public class Start implements Runnable {
 	private UltrasoundSensor us;
@@ -13,16 +14,26 @@ public class Start implements Runnable {
 	private SensorArm arm;
 	private LightSensor light;
 	private static final int LINE = 35;
+	private static final int DISTANCETOWALL = 15;
+	private boolean running;
 	public Start() {
 		us = new UltrasoundSensor(SensorPort.S3);
 		light = new LightSensor(SensorPort.S4);
 		arm = new SensorArm();
 		track = new TrackSuspension();
-		track.setSpeed(1000);
+		track.setSpeed(2000);
+		running = true;
 	}
 	@Override
 	public void run() {
-		while(us.getMeasurment() >  UltrasoundSensor.MAX_DISTANCE) {
+	 while(running) {
+		arm.turnToPosition(SensorArm.MAXLEFT);
+		driveNextToLeftWall();
+		track.stop();
+		track.pivotAngleLeft(90);
+		track.waitForMotors();
+		arm.turnToCenter();
+		while(us.getMeasurment() >=  DISTANCETOWALL) {
 			if(!track.motorsMoving()) {
 				track.forward();
 			}
@@ -30,19 +41,8 @@ public class Start implements Runnable {
 		track.stop();
 		track.pivotAngleLeft(90);
 		track.waitForMotors();
-		while(us.getMeasurment() >  UltrasoundSensor.MAX_DISTANCE) {
-			if(!track.motorsMoving()) {
-				track.forward();
-			}
-		}
-		track.stop();
-		track.pivotAngleLeft(90);
-		track.waitForMotors();
-		while(us.getMeasurment() >  UltrasoundSensor.MAX_DISTANCE) {
-			if(!track.motorsMoving()) {
-				track.forward();
-			}
-		}
+		arm.turnToPosition(SensorArm.MAXRIGHT);
+		driveNextToRightWall();
 		track.stop();
 		track.pivotAngleRight(90);
 		track.waitForMotors();
@@ -53,5 +53,58 @@ public class Start implements Runnable {
 		}
 		track.stop();
 	}
+	}
+	public void halt() {
+		running = false;
+	}
 	
+	private void driveNextToLeftWall() {
+		boolean leftWallFound = true;
+		while(leftWallFound) {
+			if(!track.motorsMoving()) {
+				track.forward();
+			}
+			track.setSpeed(2000);
+			int measure = us.getMeasurment();
+			RConsole.println(String.valueOf(measure));
+			if(measure < 20) {
+				track.setSpeedRight(100);
+			}
+			if(measure > 25 && measure < UltrasoundSensor.MAX_DISTANCE) {
+				track.setSpeedLeft(100);
+			}
+			if(measure == UltrasoundSensor.MAX_DISTANCE) {
+				leftWallFound = false;
+			}
+			sleep(200);
+		}
+		}
+		private void driveNextToRightWall() {
+			boolean rightWallFound = true;
+			while(!rightWallFound) {
+			if(!track.motorsMoving()) {
+				track.forward();
+			}
+			track.setSpeed(2000);
+			int measure = us.getMeasurment();
+			RConsole.println(String.valueOf(measure));
+			if(measure < 20) {
+				track.setSpeedLeft(100);
+			}
+			if(measure > 25 && measure < UltrasoundSensor.MAX_DISTANCE) {
+				track.setSpeedRight(100);
+			}
+			if(measure == UltrasoundSensor.MAX_DISTANCE) {
+				rightWallFound = false;
+			}
+			sleep(200);
+		  }
+		}
+		private static void sleep(int millis) {
+			try {
+				Thread.sleep(millis);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 }
