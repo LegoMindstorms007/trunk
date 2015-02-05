75c150f29e8252bc47a7e8d1ec8bd2abcb45cb95
Mark <uycvo@student.kit.edu>
2015-02-05 12:09:22 +0100

    Work on CollisionDetectionUS

--- Gruppe7/src/Programs/CollisionDetectionUS.java
+++ Gruppe7/src/Programs/CollisionDetectionUS.java
@@ -1,9 +1,12 @@
 package Programs;
 
+import Sensors.UltrasoundSensor;
+
 public class CollisionDetectionUS implements Program{
 	private boolean running;
 	private boolean possibleCollsion;
 	private boolean collided;
+	private UltrasoundSensor us = UltrasoundSensor.getInstanceOf();
 	@Override
 	public void run() {
 		while(running) {
@@ -19,11 +22,33 @@ public class CollisionDetectionUS implements Program{
 	
 	private void checkForCollision() {
 		boolean checking = true;
+		int distance = 0;
 		while(checking && running) {
-			//if()
+			distance = us.getAverageMeasurement(10);
+			if(distance < 10) {
+				possibleCollsion = true; 
+				checking = false;
+			}
 		}
+		sleep(500);
+		if(us.getAverageMeasurement(10) <= distance) {
+			possibleCollsion = false;
+		} else {
+			collided = true;
+		}
+		
 	}
 	
+	private void sleep(int i) {
+	try {
+		Thread.sleep(i);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+	}
+
 	public boolean collided() {
 		return collided;
 	}
