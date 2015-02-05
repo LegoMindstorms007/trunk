6219881f8dcdf4628c6c0b4eb552477d6fc42977
Mark <uycvo@student.kit.edu>
2015-02-05 10:23:59 +0100

    TurnTableProgram fixes

--- Gruppe7/src/Test/TurnTableTester.java
+++ Gruppe7/src/Test/TurnTableTester.java
@@ -6,7 +6,7 @@ import Programs.TurnTableProgram;
 
 public class TurnTableTester {
 	public static void main(String[] args) {
-		TurnTableProgram turn = new TurnTableProgram(SensorPort.S4, SensorPort.S3);
+		TurnTableProgram turn = new TurnTableProgram();
 		new Thread(turn).start();
 		Button.waitForAnyPress();
 		turn.halt();
