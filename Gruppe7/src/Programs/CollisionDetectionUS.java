9143ad98f859d9d74d8910ccd0a6bc67afcd4c57
Mark <uycvo@student.kit.edu>
2015-02-05 11:43:40 +0100

    Start on CollisionDetectionUS

---
+++ Gruppe7/src/Programs/CollisionDetectionUS.java
@@ -0,0 +1,41 @@
+package Programs;
+
+public class CollisionDetectionUS implements Program{
+	private boolean running;
+	private boolean possibleCollsion;
+	private boolean collided;
+	@Override
+	public void run() {
+		while(running) {
+			
+		}
+		
+	}
+
+	public boolean possibleCollision() {
+		return possibleCollsion;
+		
+	}
+	
+	private void checkForCollision() {
+		boolean checking = true;
+		while(checking && running) {
+			if()
+		}
+	}
+	
+	public boolean collided() {
+		return collided;
+	}
+	@Override
+	public void halt() {
+		running = false;
+		
+	}
+
+	@Override
+	public boolean isRunning() {
+		return running;
+	}
+
+}
